7e67915efeb6d9e70cb13d94e21be5d4f013e1a4
ziedAllouen <uchihazied@gmail.com>
2023-03-14 14:48:33 +0100

    controller crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [ConfigModule.forRoot(), TasksModule], //configModule allow us to use environement variables
+  controllers: [AppController],
+  providers: [AppService],
 
 })
 export class AppModule {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get ,Put, Delete, Query } from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
+import { TasksService } from './tasks.service';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('Tasks')
+export class TasksController {
+    constructor(private readonly seeTasks:TasksService){}
+
+    //add new task
+    @Post()
+    AddTask(){
+        return "Add new task";
+    }
+    
+    //get all tasks
+    @Get()
+    ShowTasks(){
+        return "Tasks";
+    }
+
+    //get one task
+    @Get('/:id')
+    showOneTask(@Param() {id}){
+        return `Task of today ${id}`
+    }
+
+    //update a task 
+    @Put('/:id')
+    UpdateTask(@Param() {id}){
+        return `the task with the id: ${id} is updated`
+    }
+
+    //delete a task
+    @Delete('/:id')
+    DeleteTask(@Param(){id}){
+        return `the task with the id ${id} is now deleted`
+    }
+
+    //search a task
+    @Post('/search')
+    Search(@Query() {key}){
+        return key ;
+    }
+
+}
