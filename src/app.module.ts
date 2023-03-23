b40638c805f702bf62b6c543c816b17b3d1aeece
ziedAllouen <uchihazied@gmail.com>
2023-03-23 15:57:04 +0100

    authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TestModule } from './test/test.module';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MDB), 
-    TasksModule, TestModule, AuthModule], 
+    TasksModule, AuthModule], 
   controllers: [AppController],
   providers: [AppService],
 
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,46 +1,49 @@
 import { TaskDto } from './../dto/tasks.dto';
-import { Controller, Post, Get ,Put, Delete, Query } from '@nestjs/common';
-import { Body, Param } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Controller, Post, Get, Put, Delete, Query } from '@nestjs/common';
+import {
+  Body,
+  Param,
+} from '@nestjs/common/decorators/http/route-params.decorator';
 import { TasksService } from './tasks.service';
 
 @Controller('Tasks')
 export class TasksController {
-    constructor(private readonly seeTasks:TasksService){}
+  constructor(private readonly seeTasks: TasksService) {}
 
-    //add new task
-    @Post()
-    AddTask(@Body() body: TaskDto){
-        return this.seeTasks.Add(body)
-    }
-    
-    //get all tasks
-    @Get()
-    ShowTasks(){
-        return this.seeTasks.ShowAll()
-    }
+  //add new task
+  @Post()
+  AddTask(@Body() body: TaskDto) {
+    //assign the request recived on @Body to body
+    return this.seeTasks.Add(body);
+  }
 
-    //get one task
-    @Get('/:id')
-    showOneTask(@Param() {id}){
-        return this.seeTasks.ShowOne(id)
-    }
+  //get all tasks
+  @Get()
+  ShowTasks() {
+    return this.seeTasks.ShowAll();
+  }
 
-    //update a task 
-    @Put('/:id')
-    UpdateTask(@Param() {id},@Body() body:TaskDto){
-        return this.seeTasks.Update(id,body)
-    }
+  //get one task
+  @Get('/:id')
+  showOneTask(@Param('id') id: string) {
+    return this.seeTasks.ShowOne(id);
+  }
 
-    //delete a task
-    @Delete('/:id')
-    DeleteTask(@Param(){id}){
-        return this.seeTasks.Delete(id)
-    }
+  //update a task
+  @Put('/:id')
+  UpdateTask(@Param('id') id: string, @Body() body: TaskDto) {
+    return this.seeTasks.Update(id, body);
+  }
 
-    //search a task
-    @Post('/search')
-    Search(@Query() {key}){
-        return this.seeTasks.Search(key)
-    }
+  //delete a task
+  @Delete('/:id')
+  DeleteTask(@Param('id') id: string) {
+    return this.seeTasks.Delete(id);
+  }
 
+  //search a task
+  @Post('/search')
+  Search(@Query() { key }) {
+    return this.seeTasks.Search(key);
+  }
 }
